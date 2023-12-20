9de9650adff5233fee96550f44755a71b3f995c7
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2023-12-21 01:04:23 +0300

    canister updated

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,22 @@
-import { Canister, query, text, update, Void, 
-    Record, StableBTreeMap, Vec, 
-    Result, nat64, ic, Opt, int8, int16, int64, Principal,
-} from 'azle';
-import { v4 as uuidv4 } from 'uuid';
+import { Canister, query, text, update, Result, StableBTreeMap, int64 } from 'azle';
 
 type User = {
-    id: int64,
-    uname: text,
-    password: text,
-    status: text,
-    money: int64,
-    property: Property | null,
+  id: int64,
+  uname: text,
+  password: text,
+  status: text,
+  money: int64,
+  property: Property | null,
 };
 
 type Property = {
-    pname?: text,
-    value?: int64,
-    percentage?: int64,
-    shareHolders?: {
-      name: text,
-      percentages: int64,
-    }[]
+  pname?: text,
+  value?: int64,
+  percentage?: bigint,
+  shareHolders?: {
+    name: text,
+    percentages: bigint,
+  }[]
 };
 
 const UserDB = StableBTreeMap<int64, User>(0);
@@ -31,15 +27,14 @@ function randomNumber(): int64 {
 }
 
 // Helper function to generate the deduction of money for investment
-function newMoneyGenerator(user: any, company: any, percentage: bigint) {
-  const cost = BigInt(company.Some.property.value) * BigInt(percentage) / BigInt(100);
+function newMoneyGenerator(user: User, company: User, percentage: bigint): int64 {
+  const cost = (company?.Some?.property?.value || BigInt(0)) * (percentage || BigInt(0)) / BigInt(100);
   const newMoney = user.Some.money - cost;
   return newMoney;
 }
 
-
 export default Canister({
-  createUser: update([text, text, text, int64], text, (name, password, status, money) => {
+  createUser: update([text, text, text, int64], Result(text, 'An unexpected error occurred.'), (name, password, status, money) => {
     const id = randomNumber();
 
     UserDB.insert(id, {
@@ -52,33 +47,33 @@ export default Canister({
     });
 
     // User gets to choose their status as 'Manager' or 'User'
-    if(status == "Manager") {
+    if (status === 'Manager') {
       return `Create your first property by using the command 'createProperty', you will need your id: ${id}`;
     }
 
-      return `Your account is created successfully! Please keep this id for further interactions: ${id}`;
+    return `Your account is created successfully! Please keep this id for further interactions: ${id}`;
   }),
 
-  checkBalance: query([int64], text, (id) => {
+  checkBalance: query([int64], Result(text, 'Your id is incorrect!'), (id) => {
     const user = UserDB.get(id);
 
-    if('None' in user) {
-      return 'Your id is incorrect!';
+    if ('None' in user) {
+      return Result.Err('Your id is incorrect!');
     } else {
       const money = user.Some.money;
-      return `your current balance is ${money}`;
+      return Result.Ok(`Your current balance is ${money}`);
     }
   }),
 
-  createProperty: update([int64, text, int64], text, (id, propertyName, value) => {
+  createProperty: update([int64, text, int64], Result(text, 'Your id is wrong!'), (id, propertyName, value) => {
     const user = UserDB.get(id);
 
-    if('None' in user) {
-      return 'Your id is wrong!';
+    if ('None' in user) {
+      return Result.Err('Your id is wrong!');
     }
 
-    if(user.Some.status == 'User') {
-      return 'A user cannot make a property! Make an account with a Manager status.';
+    if (user.Some.status === 'User') {
+      return Result.Err('A user cannot make a property! Make an account with a Manager status.');
     }
 
     UserDB.insert(id, {
@@ -87,79 +82,77 @@ export default Canister({
         pname: propertyName,
         value: value,
         percentage: BigInt(100),
-      }
+      },
     });
 
-    return `Your property is created successfully! shareholders will refer to your id to invest: ${id}`;
+    return Result.Ok(`Your property is created successfully! Shareholders will refer to your id to invest: ${id}`);
   }),
 
-  investProperty: update([int64, int64, int64], text, (userID, companyID, percentage) => {
-    const userResult = UserDB.get(userID);
-    const companyResult = UserDB.get(companyID);
-  
-    if ('None' in userResult) {
-      return 'Your account ID is wrong!';
-    }
-  
-    if ('None' in companyResult) {
-      return 'The property you were looking for is not listed here!';
-    }
-  
-    if (!userResult.Some || !companyResult.Some) {
-      return 'An unexpected error occurred.';
-    }
-  
-    if (!companyResult.Some.property) {
-      return 'The property details are missing!';
+  investProperty: update([int64, int64, int64], Result(text, 'An unexpected error occurred.'), (userId, companyId, percentage) => {
+    const userResult = UserDB.get(userId);
+    const companyResult = UserDB.get(companyId);
+
+    if ('None' in userResult || 'None' in companyResult) {
+      return Result.Err('Your account ID or property ID is wrong!');
     }
 
-    if (companyResult.Some.property.percentage === undefined) {
-      return 'Percentage of property is not defined!';
+    const user = userResult.Some;
+    const company = companyResult.Some;
+
+    if (!user || !company || !company.property || company.property.percentage === undefined) {
+      return Result.Err('An unexpected error occurred.');
     }
-    
-    const name: string = userResult.Some.uname;
-    let newMoney: int64 = newMoneyGenerator(userResult, companyResult, BigInt(percentage));
-  
-    let newPercentage: int64 = companyResult.Some.property.percentage - BigInt(percentage);
-  
-    let currentShareHolders: { name: string; percentages: bigint }[] = companyResult.Some.property.shareHolders || [];
+
+    if (percentage <= 0 || percentage > company.property.percentage) {
+      return Result.Err('Invalid percentage to invest!');
+    }
+
+    let newMoney = newMoneyGenerator(user, company, BigInt(percentage));
+
+    if (newMoney < 0) {
+      return Result.Err('Not enough money to invest!');
+    }
+
+    const name: string = user.uname;
+
+    let newPercentage: bigint = company.property.percentage - BigInt(percentage);
+
+    let currentShareHolders: { name: string; percentages: bigint }[] = company.property.shareHolders || [];
     const newShareHolder = {
       name: name,
       percentages: BigInt(percentage),
     };
-  
+
     currentShareHolders.push(newShareHolder);
-  
-    UserDB.insert(companyID, {
-      ...companyResult.Some,
+
+    UserDB.insert(companyId, {
+      ...company,
       property: {
-        ...companyResult.Some.property,
+        ...company.property,
         percentage: newPercentage,
-        shareHolders: currentShareHolders
-      }
+        shareHolders: currentShareHolders,
+      },
     });
-  
-    UserDB.insert(userID, {
-      ...userResult.Some,
+
+    UserDB.insert(userId, {
+      ...user,
       money: newMoney,
     });
-  
-    return 'Investment updated successfully!';
-  })
-  
-   
+
+    return Result.Ok('Investment updated successfully!');
+  }),
 });
 
 // a workaround to make uuid package work with Azle
 globalThis.crypto = {
-    // @ts-ignore
-    getRandomValues: () => {
-      let array = new Uint8Array(32);
-  
-      for (let i = 0; i < array.length; i++) {
-        array[i] = Math.floor(Math.random() * 256);
-      }
-  
-      return array;
-    },
-  };
+  // @ts-ignore
+  getRandomValues: () => {
+    let array = new Uint8Array(32);
+
+    for (let i = 0; i < array.length; i++) {
+      array[i] = Math.floor(Math.random() * 256);
+    }
+
+    return array;
+  },
+};
